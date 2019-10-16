28d47298d1955540d67cbfb77824145b86c59e86
Vladyslav Ivanov <vladyslav.iva@gmail.com>
2019-10-15 21:24:54 -0700

    Add single layer neural network

--- main.py
+++ main.py
@@ -1,36 +1,58 @@
-import numpy as np
-
-true_w = np.array([1, 2, 3, 4, 5])
-d = 5
-points = []
-
-for i in range(10000):
-    x = np.random.randn(5)
-    y = true_w.dot(x) + np.random.randn()
-    points.append((x, y))
-
-def sF(w, i):  # Loss(x_i, y_i, w)
-    x, y = points[i]
-    return (w.dot(x) - y)**2
+"""
+Date: October 15th, 2019
+Author: Vladyslav Ivanov <vladyslav.iva@gmail.com>
+Description: Neural Network implementation in pure Python
+"""
+import math
+import random
 
 
-def sdF(w, i):  # gradient
-    x, y = points[i]
-    return 2 * (w.dot(x) - y) * x
+class NeuralNetwork():
+
+    def __init__(self, num_predictors):
+        self.weights = [random.uniform(-1, 1) for _ in range(num_predictors)]
+
+    def sigmoid(self, value_array):
+        return [1 / (1 + math.pow(math.e, -x)) for x in value_array]
+
+    def sigmoid_derivative(self, xarray):
+        return [x * (1 - x) for x in xarray]
+
+    def dot(self, m0, m1):
+        return [sum([x * y for x, y in zip(m0, vec)]) for vec in m1]
+
+    def multiply(self, m0, m1):
+        return [x * y for x, y in zip(m0, m1)]
+
+    def difference(self, m0, m1):
+        return [x - y for x, y in zip(m0, m1)]
+
+    def add(self, m0, m1):
+        return [x + y for x, y in zip(m0, m1)]
+
+    def transpose(self, xarray):
+        return list(zip(*xarray))
+
+    def train(self, inputs, outputs, epochs):
+        for _ in range(epochs):
+            dendrites = self.sigmoid(self.dot(self.weights, inputs))
+            residuals = self.difference(outputs, dendrites)
+            weight_adjustments = self.multiply(self.sigmoid_derivative(dendrites), residuals)
+            self.weights = self.add(self.weights, self.dot(weight_adjustments, self.transpose(inputs)))
+
+    def predict(self, inputs):
+        return self.sigmoid(self.dot(self.weights, inputs))
 
 
-def stochasticGradientDescent(sF, sdF, d, n):
-    w = np.zeros(d)
-    num_updates = 0
-    eta = 0.01
-    for t in range(500):
-        for i in range(n):
-            value = sF(w, i)
-            gradient = sdF(w, i)
-            num_updates += 1
-            eta = 1.0 / num_updates
-            w = w - eta * gradient  # Key: take a step
-        print('numIters = %d, weight = %s, Training Loss Value = %s, gradient = %s' % (t, w, value, gradient))
+if __name__ == "__main__":
+    # Training Data
+    x_train = [[0, 0, 0, 0], [1, 0, 0, 0], [1, 1, 0, 0], [0, 0, 1, 1], [0, 0, 0, 1]]
+    y_train = [0, 0, 0, 1, 1]
 
+    # Test Data
+    x_test = [[0, 1, 0, 0]]
+    epochs = 1000
 
-stochasticGradientDescent(sF, sdF, d, len(points))
+    model = NeuralNetwork(len(x_train[0]))
+    model.train(x_train, y_train, epochs)
+    result = model.predict(x_test)
